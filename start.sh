fb304ff09a30397ced0d1f0b1e85627a88010a57
Daniel J. Pritchett <dpritchett@gmail.com>
2020-03-12 12:43:28 -0500

    Adds more slack-related env vars for events api support

--- start.sh
+++ start.sh
@@ -2,7 +2,11 @@
 
 docker run --rm \
   --name mybot \
-  --env SLACK_TOKEN=$SLACK_TOKEN \
+  --env SLACK_TOKEN \
+  --env SLACK_WORKSPACE_TOKEN \
+  --env SLACK_VERIFICATION_TOKEN \
+  --env SLACK_EVENTS_CALLBACK_PATH \
+  --env SLACK_INTERACTIONS_CALLBACK_PATH \
   -v "$PWD"/config:/config \
   target/flottbot:latest \
   /flottbot
